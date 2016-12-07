70250c47d38293077e7d67712c19ca75166953b7
theAirC <theairc@gmail.com>
2016-12-07 14:31:25 +0200

    changed Button_t from u8 to bool

--- UTREX_Link/Core/Types.h
+++ UTREX_Link/Core/Types.h
@@ -1,15 +1,15 @@
 #pragma once
 
-typedef s32 Delta_t;  // Relative values, like mouse movement or trackball change.
-typedef u16 Range_t;  // Absolute values, like throttles sticks, knobs and joysticks.
-typedef u8  State_t;  // Distinct state values, like multi-position switches and D-pads.
-typedef u8  Button_t; // Simple buttons, 1 or 0
+typedef s32  Delta_t;  // Relative values, like mouse movement or trackball change.
+typedef u16  Range_t;  // Absolute values, like throttles sticks, knobs and joysticks.
+typedef u8   State_t;  // Distinct state values, like multi-position switches and D-pads.
+typedef bool Button_t; // Simple buttons, 1 or 0
 // typedef bits Buttons_t
 
 const Delta_t  Delta_Center = 0;
 const Range_t  Range_Center = 32 * 1024 - 1;
 const State_t  State_Center = 0;
-const Button_t Button_Center = 0;
+const Button_t Button_Center = false;
 //Buttons_t Buttons_Center(1);
 
 // The following Mixer values are used internally,
